66276621a7b89105443518c505e958a6ccb83c01
Gustavo Fumio Oyakawa <ra117150@kraft.lab.ic.unicamp.br>
2015-05-20 15:57:42 -0300

    First attempt at computeBBDefUse

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 * Left operand of an instruction: http://lists.cs.uiuc.edu/pipermail/llvmdev/2010-April/030721.html and http://stackoverflow.com/questions/24434771/llvm-get-operand-and-lvalue-name-of-an-instruction
 * 
 * 
+
+## Running the pass
+* opt -S -load dce-pass-p3/Release/P3.so -liveness <input>.ll -o <output>.ll
--- dce-pass-p3/p3/Liveness.cpp
+++ dce-pass-p3/p3/Liveness.cpp
@@ -14,20 +14,28 @@ void print_elem(const Value* i) {
 bool Liveness::isLiveOut(Instruction *I, Value *V) {
     // Instr. trivialmente viva se:
     // seu comportamento gerar efeitos colaterais;
-    if (I->mayHaveSideEffects())
-         return true;
+    if (I->mayHaveSideEffects()) {
+        //errs() << "Side effects! ";
+        return true;
+    }
     
     // for um terminador;
-    if (isa<TerminatorInst>(I))
-         return true;
+    if (isa<TerminatorInst>(I)) {        
+        //errs() << "Terminator! ";
+        return true;
+    }
     
     // for instr. de debugging;
-    if (isa<DbgInfoIntrinsic>(I))
-         return true;
+    if (isa<DbgInfoIntrinsic>(I)) {
+        //errs() << "Debug instruction! ";
+        return true;
+    }
     
     // for instr. de excecao;
-    if (isa<LandingPadInst>(I))
-         return true;
+    if (isa<LandingPadInst>(I)) {
+        //errs() << "Exception instruction! ";
+        return true;
+    }
     
     // TODO: for usada por outra instr. que está viva
     
@@ -36,21 +44,41 @@ bool Liveness::isLiveOut(Instruction *I, Value *V) {
 
 void Liveness::computeBBDefUse(Function &F) {
     // iterando em BBs
-    for (Function::iterator i = F.begin(), e = F.end(); i != e; ++i) {
-        errs() << "Basic block (name=" << i->getName() << ") has " << i->size() << " instructions.\n";
+    for (Function::iterator blk = F.begin(), e = F.end(); blk != e; ++blk) {
+        LivenessInfo li;
+        // iterando em Is
+        for (BasicBlock::iterator i = blk->begin(), f = blk->end(); i != f; ++i) {
+            // adiciona própria instrução no conjunto def
+            // errs() << "Block " << blk->getName() << " defines " << *i << "\n";
+            li.def.insert(i);
+            // adiciona operandos no conjunto use
+            for (User::op_iterator op = i->op_begin(), x = i->op_end(); op != x; ++op){
+                // errs() << *i << " Block " << blk->getName() << " uses " << *op << "\n";
+                li.use.insert(*op);
+            }
+        }
+        bbLivenessMap.insert(std::pair<const BasicBlock*, LivenessInfo>(blk,li));
+        // TODO check if maps entries are being set correctly
     }
 }
 
 void Liveness::computeBBInOut(Function &F) {
     // iterando em BBs
-    for (Function::iterator i = F.begin(), e = F.end(); i != e; ++i) {}
+    for (Function::iterator blk = F.begin(), e = F.end(); blk != e; ++blk) {
+        // iterando em Is
+        for (BasicBlock::iterator i = blk->begin(), f = blk->end(); i != f; ++i) {
+            // 
+            // 
+        }
+    }
 }
 
 void Liveness::computeIInOut(Function &F) {
     // iterando em Is
     for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-        if(isLiveOut((Instruction*)&*I, NULL)) 
-            errs() << *I << " is live out!\n";
+        if (isLiveOut((Instruction*)&*I, NULL)) {
+            //errs() << *I << " is live out.\n";
+        }
     }
 }
 
@@ -64,8 +92,3 @@ bool Liveness::runOnFunction(Function &F) {
 char Liveness::ID = 0;
 
 RegisterPass<Liveness> X("liveness", "Live vars analysis", false, false);
-
-
-
-
-
